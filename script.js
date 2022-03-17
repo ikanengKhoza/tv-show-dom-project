29c8320845f437fef5fac5d3ee12b1351c59c04e
Ika Khoza <ikaneng.khoza@gmail.com>
2022-03-17 13:42:15 +0000

    level 300, 350
    completed 300, 350 and started 400

--- script.js
+++ script.js
@@ -1,78 +1,169 @@
 //You can edit ALL of the code here
+//globbal variables
+const getInput = document.getElementById("site-search");
+const searchCount = document.getElementById("display-episode-count");
+const selectElem = document.getElementById("select");
+const selectedOption = document.getElementById("showId")
+
+
+
 function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+    myFetchedData()
+    inputSearch()
+    onChangeSelect()
+    const allShows = getAllShows();
+    makePageForShowList(allShows);
+    selectShow(allShows)
+    selectOption()
+  
 }
 
 
-
+//displays all episodes on the page
 function makePageForEpisodes(episodeList) {
-  
   episodeList.forEach(function (episode) {
+  console.log(episode)  
   const rootElem = document.getElementById("root")
   const headingName = document.createElement("h3")
   const pTag = document.createElement("p") 
   const image = document.createElement("img")
   const eachEpisodeContainer = document.createElement("div")
   
-  
   headingName.innerText = `${episode.name} - S0${episode.season}E0${episode.number}`
-  
   pTag.innerHTML = `${episode.summary}`
   image.src = `${episode.image.medium}`
   
-  //appending h3 and paragraph in our newly creadted div eachEpisodeContainer
-  eachEpisodeContainer.append(headingName)
-  eachEpisodeContainer.append(image)
-  eachEpisodeContainer.append(pTag)
+  
+  eachEpisodeContainer.appendChild(headingName)
+  eachEpisodeContainer.appendChild(image)
+  eachEpisodeContainer.appendChild(pTag)
 
-  eachEpisodeContainer.className = "episodes"
-  
-  
-  //append eachEpisodeContainer to root div that was provided
+  eachEpisodeContainer.className = "episodes"  
   rootElem.appendChild(eachEpisodeContainer)
+  })
+}
+
+//make page for shows
+function makePageForShowList(episodeList) {
+  episodeList.forEach(function (show) {
+   //console.log(show) 
+  const rootElem = document.getElementById("root")
+  const headingName = document.createElement("h3")
+  const pTag = document.createElement("p") 
+  const image = document.createElement("img")
+  const eachShowContainer = document.createElement("div")
+  
+  headingName.innerText = `${show.name} 
+ \n premired: ${show.premiered} \n runtime: ${show.runtime} \n `
+  pTag.innerHTML = `${show.summary} \n \ngenres: ${show.genres}`
+  image.src = `${show.image.medium}`
+  
+  
+  eachShowContainer.appendChild(headingName)
+  eachShowContainer.appendChild(image)
+  eachShowContainer.appendChild(pTag)
+
+  eachShowContainer.className = "showlist"  
+  rootElem.appendChild(eachShowContainer)
+  })
+}
+
+
+
+//displays searched episode and episode-count inside eventlisterner
+function inputSearch()  {
+  getInput.addEventListener("keyup", (e) => {
+    const searchedEpisode = e.target.value.toLowerCase()
+    const filteredInput = currentEpisodes.filter((e) => {
+    return (
+      e.name.toLowerCase().includes(searchedEpisode) ||
+      e.summary.toLowerCase().includes(searchedEpisode)
+   )  
+ });
+   
+    const filteredCount = filteredInput.length;
+    const allCount = currentEpisodes.length;
+    const countEpisode = `displaying ${filteredCount} / ${allCount}`;
+    searchCount.innerText = countEpisode;
     
+    const rootElem = document.getElementById("root");
+    rootElem.innerHTML = "";
+    
+    makePageForEpisodes(filteredInput);
+    })
+    const displayEpisodeSearch = document.getElementById("display-episode-count")
+}
+
+
+
+//episode dropdown options
+function selectOption (options) {
+  options.forEach(element => {
+    const optionElem = document.createElement("option");
+    selectElem.appendChild(optionElem)
+    optionElem.innerHTML = `S0${element.season}E0${element.number} - ${element.name}`
+  })
+}
+
+// show dropdown options
+function selectShow(show) {
+  show.forEach(element => {
+    const showSelOption = document.createElement("option");
+    selectedOption.appendChild(showSelOption)
+    showSelOption.innerHTML = `${element.name}`
+  })
+}
+
+
+function onChangeSelect(){
+  selectedOption.addEventListener('change', (event) => {
+  const clickedEpisode = currentEpisodes.filter((e) => {
+  const result =  event.target.value; 
+    console.log(result)
+
+    return (
+      e.id.result 
+   )      
+});
+makePageForShowList(clickedEpisode);
+})
+}
+
+
+//showlist show list dropdown
+function selectShowList() {
+ fetch(`https://api.tvmaze.com/shows`)
+  .then(res => res.json())
+  .then(data => {
+    
+  selectShow(data)
   })
 
-
-
-// searchInput = document.getElementById("site-search")
-// console.log(searchInput)
-
-// searchInput.addEventListener("keyup", episodeList => {
-//   searchedString = e.target.value
-//   filteredEpisodes = episodeList.filter(episode => {
-//     return episode.name.includes(searchedString).toLowerCase() || episode.summary.includes(searchedString).toLowerCase()
-
-//   });
-//   console.log(filteredEpisodes)
-// })
-
+  .catch((e) => console.log(e));
+   
 }
 
-function onSearchKeyUp(event) {
-  const searchTerm = event.target.value.toLowerCase();
-  const allEpisodes = getAllEpisodes();
 
-  const filteredEpisodes = allEpisodes.filter((e) => {
-    const episodeName = e.name.toLowerCase();
-    const episodeSummary = e.summary.toLowerCase();
-    return (
-      episodeName.includes(searchTerm) || episodeSummary.includes(searchTerm)
-    );
-  });
+//fetch for episodes
+function myFetchedData() {
+ fetch(`https://api.tvmaze.com/shows/82/episodes`)
+  .then(response => response.json())  
+  .then(data => {
 
-  const filteredCount = filteredEpisodes.length;
-  const allCount = allEpisodes.length;
+   makePageForEpisodes(data)
+   selectOption(data)
+   selectShowList(data)
+  makePageForShowList(data)
+   currentEpisodes = data
+   onChangeSelect(data)
+   
+   
+  })
 
-  const countString = `Displaying ${filteredCount} / ${allCount}`;
-
-  searchCount.innerText = countString;
-  makePageForEpisodes(filteredEpisodes);
-}
+  .catch((e) => console.log(e));
+  }
 
 
 
 
 window.onload = setup;
-
